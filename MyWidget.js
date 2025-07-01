fcd4754bc8111b0ad75498a65074ff2fcd7f9810
AKAjaikumar <leelaajaikumar@gmail.com>
2025-07-01 13:38:35 +0530

    Update MyWidget.js

--- MyWidget.js
+++ MyWidget.js
@@ -6,8 +6,12 @@ define("hellow", [
 ], function (Core, Alone, WAFData, PlatformAPI) {
   var myWidget = {
     onLoadWidget: function () {
-      widget.body.innerHTML = "";
-      console.log("widget loaded");
+		widget.body.innerHTML = "";
+		console.log("widget loaded");
+		require(["DS/DataGridView/DataGridView"], function (DataGridView) {
+			console.log("DataGridView loaded:", DataGridView);
+		});
+		
     }
   };
 
