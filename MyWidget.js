6512858383a11bf048e144e8b0c9cbe446f91b06
AKAjaikumar <leelaajaikumar@gmail.com>
2025-07-01 14:37:51 +0530

    Update MyWidget.js

--- MyWidget.js
+++ MyWidget.js
@@ -3,14 +3,14 @@ define("hellow", [
   "UWA/Drivers/Alone",
   "DS/WAFData/WAFData",
   "DS/PlatformAPI/PlatformAPI",
-  "DS/Tree/Tree"
-], function (Core, Alone, WAFData, PlatformAPI, Tree) {
+  "UWA/Controls/DataGrid",
+], function (Core, Alone, WAFData, PlatformAPI, DataGrid) {
   var myWidget = {
     onLoadWidget: function () {
 		widget.body.innerHTML = "";
 		console.log("widget loaded");
 		
-		console.log("Tree loaded:", Tree);
+		console.log("DataGrid loaded:", DataGrid);
 		
     }
   };
