f3f55c74cc338949e6a99fbf5e775b909e36384b
AKAjaikumar <leelaajaikumar@gmail.com>
2025-07-01 12:53:45 +0530

    Update MyWidget.js

--- MyWidget.js
+++ MyWidget.js
@@ -1,14 +1,15 @@
-define("hellow", ["UWA/Core",
+define("hellow", [
+  "UWA/Core",
   "UWA/Drivers/Alone",
   "DS/WAFData/WAFData",
-  "DS/PlatformAPI/PlatformAPI"], function (Core, Alone, WAFData,PlatformAPI) {
-	   var myWidget = {
-        onLoadWidget: function () {
-            widget.body.innerHTML = "";
-			console.log("widget loaded");
-		}
-		    return myWidget;
-			
-		};
-	
-	});
+  "DS/PlatformAPI/PlatformAPI"
+], function (Core, Alone, WAFData, PlatformAPI) {
+  var myWidget = {
+    onLoadWidget: function () {
+      widget.body.innerHTML = "";
+      console.log("widget loaded");
+    }
+  };
+
+  return myWidget;
+});
