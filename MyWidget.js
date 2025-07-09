04e0e081767f7e0529a2837f4587ac4487df10aa
AKAjaikumar <leelaajaikumar@gmail.com>
2025-07-09 15:27:15 +0530

    Update MyWidget.js

--- MyWidget.js
+++ MyWidget.js
@@ -369,7 +369,7 @@ function callEINWebService(selectedIds, onComplete, onError) {
           onComplete: function (csrfData) {
             const csrfToken = csrfData.csrf.value;
             const csrfHeader = csrfData.csrf.name;
-				  WAFData.authenticatedRequest('https://3e966064bcb5.ngrok-free.app/api/hello/setEIN?ObjectID='+selectedIds, {
+				  WAFData.authenticatedRequest('http://localhost:9090/SetEINRestApp/api/hello/setEIN?ObjectID='+selectedIds, {
 					method: 'POST',
 					type: 'json',
 					headers: {
